d939485cf39dc9d4b9eeae5760a14d6c497c3896
Hakan Akkas <h.akkas@hetmml.nl>
2018-03-01 10:46:23 +0100

    eerste versie Terminal Hacker

---
+++ Assets/Hacker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+    int level = 0;
+
+	// Use this for initialization
+	void Start () {
+		Terminal.WriteLine("Hi");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnUserInput(string input) {
+        Terminal.WriteLine("Je zei: " + input);
+    }
+}
